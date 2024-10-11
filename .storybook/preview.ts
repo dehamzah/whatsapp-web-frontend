1a1af9fb2a35674b05fa7c459f5c686caa2ffadf
Dede Hamzah <dehamzah@users.noreply.github.com>
2024-10-11 08:27:01 +0700

    feat: update basic chat bubble ui with props and story (#6)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+// this is needed to import the css in our app to storybook preview app
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
